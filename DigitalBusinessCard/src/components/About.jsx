8e0706cb76b0eb24177c2d0f50d4e08c644760de
Jan-Kristian Karlsen <jan-kristian@Jan-Kristians-MacBook-Air.local>
2024-09-12 13:15:41 +0200

    Created an about portion in the card

---
+++ DigitalBusinessCard/src/components/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+        <section className="about">
+            <div className="about-content">
+                <h2 className="about-title">About</h2>
+                <p className="about-text">I am a frontend developer with a passion for creating beautiful and intuitive user interfaces. I have experience with React, JavaScript, HTML, and CSS. I am always looking to learn new things and improve my skills. I try to keep up with security and best practices, and am always looking for new things to learn</p>
+            </div>
+            <div className="interests-content">
+                <h2 className="interests-title">Interests</h2>
+                <p className="interests-text">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </div>       
+        </section>
+
+    )
+
+}
