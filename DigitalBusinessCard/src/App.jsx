198b3d9fc74bdd25237c300821d6809a2a6b1edb
Jan-Kristian Karlsen <jan-kristian@Jan-Kristians-MacBook-Air.local>
2024-09-12 13:26:29 +0200

    added a footer. Card is complete

--- DigitalBusinessCard/src/App.jsx
+++ DigitalBusinessCard/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import About from './components/About'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='business-card'>
       <Header />
       <About />
+      <Footer />
     </div>
   )
 }
---
+++ DigitalBusinessCard/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-socials">
+                <img src="src/assets/Twitter Icon.png" alt="Twitter icon" />
+            </div>
+            <div className="footer-socials">
+                <img src="src/assets/Facebook Icon.png" alt="Facebook Icon" />
+            </div>
+            <div className="footer-socials">
+                <img src="src/assets/Instagram Icon.png" alt="Instagram icon" />
+            </div>
+            <div className="footer-socials">
+                <img src="src/assets/GitHub Icon.png" alt="GitHub Icon" />
+            </div>
+        </footer>
+    )
+
+
+}
